2c7f33707fea4564e2f16a43ecbbd3d6a1470a7b
youngvctr <jhk91@yonsei.ac.kr>
2023-01-03 22:05:14 +0900

    Initial commit

---
+++ dist/_nuxt/static/1672751045/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhunyohuny\u002F_nuxt\u002Fstatic\u002F1672751045",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhunyohuny\u002F",assetsPath:"\u002Fhunyohuny\u002F_nuxt\u002F",cdnURL:a}}}}(null));
